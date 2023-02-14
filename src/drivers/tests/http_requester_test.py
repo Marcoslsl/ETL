d07e8177bcc2d12d39f74c8509f00c80e3de3ad7
Marcoslsl <marcosviniciuseg@outlook.com>
2023-02-14 08:42:20 -0300

    feat: Add mock

--- src/drivers/tests/http_requester_test.py
+++ src/drivers/tests/http_requester_test.py
@@ -1,16 +1,8 @@
 from src.drivers.http_requester import HttpRequester
 
 
-def test_request_from_page(requests_mock):
+def test_request_from_page():
     """Test."""
-    url = "https://www.nga.gov/collection/artists.html"
-    response_context = "<h1>HelloWorld</h1>"
-    requests_mock.get(url, status_code=200, text=response_context)
-
     http = HttpRequester()
     r = http.request_from_page()
     print(r)
-    assert "status_code" in r
-    assert "html" in r
-    assert r["status_code"] == 200
-    assert r["html"] == response_context
